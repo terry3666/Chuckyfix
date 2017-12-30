610d67e8d222fedd046f6eacc2b7bf3621446efb
raika44 <33775480+raika44@users.noreply.github.com>
2017-12-30 12:50:19 +0700

    Update Talk.py

--- LINETCR/Api/Talk.py
+++ LINETCR/Api/Talk.py
@@ -20,8 +20,8 @@ class Talk:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/1.4.17"
-  LA = "IOSIPAD\x097.14.0\x09iPhone_OS\x0910.12.0"
+  UA = "Line/7.14.0"
+  LA = "CHROMEOS\t1.4.17\tChrome_OS\t1"
 
   authToken = None
   cert = None
